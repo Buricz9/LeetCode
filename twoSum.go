8aa882dbd4d6b27be329fa9642b713c5a43ad991
buricz9 <buricz1906@gmail.com>
2025-02-27 11:57:14 +0100

    Done https://leetcode.com/problems/two-sum/description/

---
+++ twoSum.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+	println(twoSum([]int{2, 7, 11, 15}, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
