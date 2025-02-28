45a92e9b5d14b23d123d54d8737f0229bfabc94d
buricz9 <buricz1906@gmail.com>
2025-02-28 13:06:41 +0100

    small adjustment

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
--- twoSum.go
+++ twoSum.go
@@ -1,12 +1,5 @@
 package main
 
-import "fmt"
-
-func main() {
-
-	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
-}
-
 func twoSum(nums []int, target int) []int {
 	seen := make(map[int]int)
 
