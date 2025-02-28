014cda3abe2bc0f33613ec5d7117aa182d7f9093
buricz9 <buricz1906@gmail.com>
2025-02-28 15:48:33 +0100

    done addTwoNumber.go

---
+++ addTwoNumber.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	current := dummyHead
+	carry := 0
+
+	for l1 != nil || l2 != nil || carry != 0 {
+		sum := carry
+
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		carry = sum / 10
+		current.Next = &ListNode{Val: sum % 10}
+		current = current.Next
+	}
+
+	return dummyHead.Next
+}
+
+func createList(values []int) *ListNode {
+	if len(values) == 0 {
+		return nil
+	}
+
+	head := &ListNode{Val: values[0]}
+	current := head
+	for _, v := range values[1:] {
+		current.Next = &ListNode{Val: v}
+		current = current.Next
+	}
+
+	return head
+}
+
+func printList(node *ListNode) {
+	for node != nil {
+		fmt.Print(node.Val)
+		if node.Next != nil {
+			fmt.Print(" -> ")
+		}
+		node = node.Next
+	}
+	fmt.Println()
+}
--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(isPalindrome(121))  // true
-	fmt.Println(isPalindrome(-121)) // false
-	fmt.Println(isPalindrome(10))   // false
-
-	fmt.Println(romanToInt("IV"))    //4
-	fmt.Println(romanToInt("IX"))    //9
-	fmt.Println(romanToInt("LVIII")) //58
-
-	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	// PalindromeNumber
+	//fmt.Println(isPalindrome(121))  // true
+	//fmt.Println(isPalindrome(-121)) // false
+	//fmt.Println(isPalindrome(10))   // false
+	//
+	// RomanToInteger
+	//fmt.Println(romanToInt("IV"))    //4
+	//fmt.Println(romanToInt("IX"))    //9
+	//fmt.Println(romanToInt("LVIII")) //58
+	//
+	// TwoSum
+	//fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	//
+	// AddTwoNumber
+	//l1 := createList([]int{2, 4, 3})
+	//l2 := createList([]int{5, 6, 4})
+	//result := addTwoNumbers(l1, l2)
+	//printList(result) //  output: 7 -> 0 -> 8
 }
